eb59ba7dc3d945eaea16e7d92507a9d167c37c7a
DaShaun <dashaun@redislabs.com>
2021-03-24 21:37:48 -0500

    update provider config

--- azuread.tf
+++ azuread.tf
@@ -1,3 +1,6 @@
 provider "azuread" {
-  # Configuration options
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
 locals {
   redisgeek_config = {
-    cluster  = azurerm_redis_enterprise_cluster.redisgeek.*
-    database = azurerm_redis_enterprise_database.redisgeek.*
+    cluster             = azurerm_redis_enterprise_cluster.redisgeek.*
+    database            = azurerm_redis_enterprise_database.redisgeek.*
+    resource_group_name = azurerm_resource_group.redisgeek.name
   }
 }
 
