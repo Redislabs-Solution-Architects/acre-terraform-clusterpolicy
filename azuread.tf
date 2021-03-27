14c63622b73dcf0c6e6d43585eb9b2cfdcf05d80
DaShaun <dashaun@redislabs.com>
2021-03-27 01:16:18 -0500

    fmt

--- azuread.tf
+++ azuread.tf
@@ -1,5 +1,5 @@
 provider "azuread" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
+  tenant_id     = var.tenant_id
 }
